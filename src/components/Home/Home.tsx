330cf60bb5c2790110b7df5e7b580902539da5ca
JoeS <sabe12070@gmail.com>
2022-03-13 16:52:38 -0700

    notes to self

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -15,6 +15,7 @@ export const Home = () => {
 };
 
 // example created from: https://github.com/netlify/netlify-identity-widget/tree/master/example/react
+// and here: https://github.com/netlify/netlify-identity-widget
 const AuthButton = () => {
   const netlifyAuth = {
     isAuthenticated: false,
@@ -68,3 +69,57 @@ const AuthButton = () => {
     </div>
   );
 };
+
+/**
+ * user data example
+ *
+ * {
+    "id":"9a82dee6-c29f-48cf-beb5-086585531563",
+    "aud":"",
+    "role":"",
+    "email":"sabe12070@gmail.com",
+    "confirmed_at":"2022-03-13T23:43:41Z",
+    "app_metadata":{
+        "provider":"google"
+    },
+    "user_metadata":{
+        "avatar_url":"https://lh3.googleusercontent.com/a-/AOh14GjPNB-cvqTu_9zG-ppJZJQGKUblwJp6FTrUHYDy=s96-c",
+        "full_name":"Joseph Sebast"
+    },
+    "created_at":"2022-03-13T23:43:41Z",
+    "updated_at":"2022-03-13T23:43:41Z"
+  }
+
+  * {
+   "id":"5fefd637352e13d74ae6bfd0",
+   "uid":null,
+   "full_name":"barbijoe",
+   "avatar":"https://avatars2.githubusercontent.com/u/13157284?v=4",
+   "avatar_url":"https://avatars2.githubusercontent.com/u/13157284?v=4",
+   "email":"sabe12070@gmail.com",
+   "affiliate_id":"",
+   "site_count":3,
+   "created_at":"2021-01-02T02:11:03.360Z",
+   "last_login":"2022-03-12T17:22:31.326Z",
+   "login_providers":[
+      "github"
+   ],
+   "onboarding_progress":{
+      "slides":"closed",
+      "notifications_read_at":"2021-12-13T14:30:00Z"
+   },
+   "slug":"Barbacoa08",
+   "sandbox":false,
+   "connected_accounts":{
+      "github":"Barbacoa08"
+   },
+   "pending_email":null,
+   "has_pending_email_verification":false,
+   "mfa_enabled":false,
+   "saml_account_id":"",
+   "saml_slug":null,
+   "preferred_account_id":"",
+   "command_bar_signed_user_id":"3887fe693c204b4759b2ce796ace9212b0489faddff7a3f5f7a6b29832b506ba",
+   "tracking_id":"5fefd637352e13d74ae6bfd0"
+}
+ */
