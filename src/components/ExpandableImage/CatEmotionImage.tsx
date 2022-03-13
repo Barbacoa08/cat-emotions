94cc09c88f7a924b15453a2e75661d4df7fd49bf
Barbacoa08 <sabe12070@gmail.com>
2022-03-12 18:43:35 -0800

    add save functionality  (#5)
    * add save functionality 
    
    it doesn't actually do anything yet, but it's cool looking!
    
    * better name for component
    
    now that it's got more functionality
    
    * linting fix

---
+++ src/components/ExpandableImage/CatEmotionImage.tsx
@@ -0,0 +1,76 @@
+import {
+  Box,
+  Button,
+  Image,
+  Modal,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+
+export const CatEmotionImage = ({ alt, src }: { src: string; alt: string }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+
+  return (
+    <Box>
+      <Image
+        alt={alt}
+        borderRadius={10}
+        cursor="zoom-in"
+        onClick={onOpen}
+        src={src}
+      />
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+
+        <ModalContent>
+          <Image
+            alt={alt}
+            fallbackSrc="https://via.placeholder.com/100"
+            objectFit="contain"
+            src={src}
+            borderRadius={10}
+            margin="auto"
+          />
+
+          <Text align="center" padding={2}>
+            {alt}
+          </Text>
+
+          <ModalFooter>
+            <Text align="center" padding={2}>
+              Is this you right now?
+            </Text>
+
+            <Button variant="outline" onClick={onClose} marginRight={10}>
+              Nah
+            </Button>
+
+            <Button
+              variant="solid"
+              onClick={() => {
+                onClose();
+                toast({
+                  title: "Saved!",
+                  // TODO: stop lying, make it happen!
+                  description:
+                    "Your Cat Emotion has been saved to your Cat Emotion History!",
+                  status: "success",
+                  duration: 9000,
+                  isClosable: true,
+                });
+              }}
+            >
+              Definitely me!
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
+};
