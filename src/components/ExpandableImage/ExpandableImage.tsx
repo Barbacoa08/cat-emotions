f11a71592b93abcbd563622af5bca296622b1e26
JoeS <sabe12070@gmail.com>
2022-03-12 18:38:10 -0800

    add save functionality
    it doesn't actually do anything yet, but it's cool looking!

--- src/components/ExpandableImage/ExpandableImage.tsx
+++ src/components/ExpandableImage/ExpandableImage.tsx
@@ -1,15 +1,19 @@
 import {
   Box,
+  Button,
   Image,
   Modal,
   ModalContent,
+  ModalFooter,
   ModalOverlay,
   Text,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 
 export const ExpandableImage = ({ alt, src }: { src: string; alt: string }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
 
   return (
     <Box>
@@ -36,6 +40,33 @@ export const ExpandableImage = ({ alt, src }: { src: string; alt: string }) => {
           <Text align="center" padding={2}>
             {alt}
           </Text>
+
+          <ModalFooter>
+            <Text align="center" padding={2}>
+              Is this you right now?
+            </Text>
+
+            <Button variant="outline" onClick={onClose} marginRight={10}>
+              Nah
+            </Button>
+
+            <Button
+              variant="solid"
+              onClick={() => {
+                onClose();
+                toast({
+                  title: "Saved!",
+                  // TODO: stop lying, make it happen!
+                  description: "Your Cat Emotion has been saved to your Cat Emotion History!",
+                  status: "success",
+                  duration: 9000,
+                  isClosable: true,
+                });
+              }}
+            >
+              Definitely me!
+            </Button>
+          </ModalFooter>
         </ModalContent>
       </Modal>
     </Box>
