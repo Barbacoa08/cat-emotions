1f08572144fb237177f57811c086fdf794abd004
JoeS <sabe12070@gmail.com>
2022-03-13 16:45:48 -0700

    experiment with `netlify-identity-widget` to see if that'll work for me

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,6 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
+import netlifyIdentity, { User } from "netlify-identity-widget";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
   return (
@@ -6,6 +8,63 @@ export const Home = () => {
       <Heading>Cat Emotions</Heading>
 
       <Text>A tracker for your emotional journey via cat pics</Text>
+
+      <AuthButton />
     </Box>
+  );
+};
+
+// example created from: https://github.com/netlify/netlify-identity-widget/tree/master/example/react
+const AuthButton = () => {
+  const netlifyAuth = {
+    isAuthenticated: false,
+    user: null as User | null,
+    authenticate(callback: (user?: User) => void) {
+      this.isAuthenticated = true;
+      netlifyIdentity.open();
+      netlifyIdentity.on("login", (user) => {
+        this.user = user;
+        callback(user);
+      });
+    },
+    signout(callback: (user?: User) => void) {
+      this.isAuthenticated = false;
+      netlifyIdentity.logout();
+      netlifyIdentity.on("logout", () => {
+        this.user = null;
+        callback();
+      });
+    },
+  };
+
+  const navigate = useNavigate();
+
+  const login = () => {
+    netlifyAuth.authenticate((user) => {
+      console.log({ user });
+      // this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  return netlifyAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button
+        onClick={() => {
+          // netlifyAuth.signout(() => navigate("/", { replace: true }));
+          netlifyAuth.signout(() => navigate("/"));
+        }}
+      >
+        Sign out
+      </button>
+    </p>
+  ) : (
+    <div>
+      <p>You are not logged in.</p>
+      <div>
+        {/* <p>You must log in to view the page at {from.pathname}</p> */}
+        <button onClick={login}>Log in</button>
+      </div>
+    </div>
   );
 };
--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,8 +1,19 @@
+import { Box, Text } from "@chakra-ui/react";
+import netlifyIdentity from "netlify-identity-widget";
 export const SignIn = () => {
+  const user = netlifyIdentity.currentUser();
+  console.log({ user });
+
   return (
-    <div data-testid="SignIn-root">
-      TODO: redirect here if user is not signed-in already. Put in
-      google+github+FB signin options
-    </div>
+    <Box data-testid="SignIn-root">
+      <Text>
+        TODO: redirect here if user is not signed-in already. Put in
+        google+github+FB signin options
+      </Text>
+
+      <Text>
+        You are logged in as <b>{user?.email || "NO ONE"}</b>
+      </Text>
+    </Box>
   );
 };
