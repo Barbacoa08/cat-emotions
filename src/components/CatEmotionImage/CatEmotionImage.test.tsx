bfcf59b6cf3db418055ec3dafdb2feff6edfcc11
JoeS <sabe12070@gmail.com>
2022-03-15 13:57:59 -0700

    add a test for the error message

--- src/components/CatEmotionImage/CatEmotionImage.test.tsx
+++ src/components/CatEmotionImage/CatEmotionImage.test.tsx
@@ -1,15 +1,25 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import cuddly from "images/cuddly.jpg";
 
 import { CatEmotionImage } from ".";
 
 describe("CatEmotionImage Component", () => {
-  it("fully renders without exploding", () => {
-    const altText = "example alt text";
-    const { container } = render(
-      <CatEmotionImage tags={["example"]} src={cuddly} alt={altText} />
+  const altText = "example alt text";
+  const exampleTags = ["example"];
+
+  let container: HTMLElement;
+  const setup = () => {
+    const { container: containerResult } = render(
+      <CatEmotionImage tags={exampleTags} src={cuddly} alt={altText} />
     );
+
+    container = containerResult;
+  };
+
+  it("fully renders without exploding", () => {
+    setup();
 
     const button = screen.getAllByRole("button");
     expect(button).toHaveLength(1);
@@ -20,6 +30,25 @@ describe("CatEmotionImage Component", () => {
     // eslint-disable-next-line
     const images = container.querySelector("button img");
     expect(images).toBeInTheDocument();
+  });
+
+  it("displays an error if no 'tags' are selected", () => {
+    setup();
+
+    const catBtn = screen.getByRole("button");
+    userEvent.click(catBtn);
+
+    const selectedOption = screen.getByLabelText(`Remove ${exampleTags[0]}`);
+    expect(selectedOption).toBeInTheDocument();
+
+    const errorText = "You must select at least one tag";
+    expect(screen.queryByText(errorText)).not.toBeInTheDocument();
+
+    const clearBtn = screen.getByLabelText("Clear selected options");
+    userEvent.click(clearBtn);
+
+    expect(selectedOption).not.toBeInTheDocument();
+    expect(screen.getByText(errorText)).toBeInTheDocument();
   });
 
   // TODO: mock `netlifyIdentity.currentUser()` from 'netlify-identity-widget'
