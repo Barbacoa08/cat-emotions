197f8acbf41c0c2685f73dfa15f1bb4afdfaf16e
Barbacoa08 <sabe12070@gmail.com>
2022-03-16 08:01:40 -0700

    UX improvements (#9)
    * add a link to the site from the readme
    
    * improve cat image selectoin
    
    add a `maxHeight`, center images (as most are not square), export all cat images as an array so that all data about each "cat" is in one singular place
    
    * make emotion selection keyboard accessible
    
    * trim overly large images
    
    * this is working, but throws an error in chrome console
    
    "Warning: findDOMNode is deprecated in StrictMode"
    
    Which is an issue with the underlying library:
    https://github.com/jedwatson/react-select
    
    * actually use tags!
    
    still got some errors in the console... but... it does work... :grimace: and I can't find a better option (not without running a LOT of code that is)
    
    * update tests for new functionality
    
    * add a test for the error message
    
    * MOR TESTS
    
    * add a, frustrating to discover, learning
    
    * add some notes for future tests
    
    that I _should_ write, but probably won't
    
    * update code coverage badges
    
    * allow user to toggle theme

--- src/components/CatEmotionImage/CatEmotionImage.test.tsx
+++ src/components/CatEmotionImage/CatEmotionImage.test.tsx
@@ -1,20 +1,71 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import cuddly from "images/cuddly.jpg";
 
 import { CatEmotionImage } from ".";
 
 describe("CatEmotionImage Component", () => {
+  const altText = "example alt text";
+  const exampleTags = ["mischievous"];
+
+  let container: HTMLElement;
+  const setup = () => {
+    const { container: containerResult } = render(
+      <CatEmotionImage tags={exampleTags} src={cuddly} alt={altText} />
+    );
+
+    container = containerResult;
+  };
+
   it("fully renders without exploding", () => {
-    const altText = "example alt text";
-    render(<CatEmotionImage tags={["example"]} src={cuddly} alt={altText} />);
+    setup();
 
-    const images = screen.getAllByRole("img");
-    expect(images).toHaveLength(1);
+    const button = screen.getAllByRole("button");
+    expect(button).toHaveLength(1);
 
-    const imgByAltText = screen.getByAltText(altText);
-    expect(imgByAltText).toBeInTheDocument();
+    const btnByAltText = screen.getByLabelText(altText);
+    expect(btnByAltText).toBeInTheDocument();
+
+    // eslint-disable-next-line
+    const images = container.querySelector("button img");
+    expect(images).toBeInTheDocument();
   });
 
+  it("displays an error if no 'tags' are selected", () => {
+    setup();
+
+    const catBtn = screen.getByRole("button");
+    userEvent.click(catBtn);
+
+    const selectedOption = screen.getByLabelText(`Remove ${exampleTags[0]}`);
+    expect(selectedOption).toBeInTheDocument();
+
+    const errorText = "You must select at least one tag";
+    expect(screen.queryByText(errorText)).not.toBeInTheDocument();
+
+    const clearBtn = screen.getByLabelText("Clear selected options");
+    userEvent.click(clearBtn);
+
+    expect(selectedOption).not.toBeInTheDocument();
+    expect(screen.getByText(errorText)).toBeInTheDocument();
+  });
+
+  it("closes the modal when the 'Nah' button is clicked", () => {
+    setup();
+
+    const catBtn = screen.getByRole("button");
+    userEvent.click(catBtn);
+
+    const closeBtn = screen.getByText("Nah");
+    userEvent.click(closeBtn);
+
+    expect(closeBtn).not.toBeVisible();
+  });
+
+  // it("allows the selection of other tags", () => {});
+
   // TODO: mock `netlifyIdentity.currentUser()` from 'netlify-identity-widget'
+  // it("returns an 'error' toast if user is not signed in and tries to submit an emotion", () => {});
+  // it("returns an 'info' and then 'success' toast if user is signed in and submits an emotion", () => {});
 });
---
+++ src/images/index.ts
@@ -0,0 +1,69 @@
+import { CatEmotionImageProps } from "components/CatEmotionImage";
+
+import atPeace from "./at-peace.jpg";
+import beingEaten from "./being-eaten.jpg";
+import cuddly from "./cuddly.jpg";
+import destructive from "./destructive-guilty-uncaring.jpg";
+import grumpy from "./grumpy.jpg";
+import happy from "./happy.jpg";
+import mischievous from "./mischievous.jpg";
+import sleepy from "./sleepy.jpg";
+import surprisedGuilty from "./surprised-guilty.jpg";
+import surprised from "./surprised.jpg";
+
+export const allCatImages: CatEmotionImageProps[] = [
+  {
+    src: atPeace,
+    tags: ["happy", "content"],
+    alt: "image of a cat on the top of a mountain breathing deeply, seemingly at peace with the world",
+  },
+  {
+    src: beingEaten,
+    tags: ["annoyed", "apathetic"],
+    alt: "image of a cat being gently chomped on by a large dog, the cat appears to not care",
+  },
+  {
+    src: cuddly,
+    tags: ["cuddly"],
+    alt: "image of a mother cat curled up and sleeping with it's kitten embraced",
+  },
+  {
+    src: destructive,
+    tags: ["destructive", "angry"],
+    alt: "image of a cat sitting on a completely destroyed chair looking guilty and completely uncaring",
+  },
+  {
+    src: grumpy,
+    tags: ["grumpy"],
+    alt: "image of grumpy cat",
+  },
+  {
+    src: happy,
+    tags: ["happy", "smiley"],
+    alt: "image of a cat staring into the camera with closed eyes and a happy smile",
+  },
+  {
+    src: mischievous,
+    tags: ["mischievous"],
+    alt: "image of a cat with a paw up and a mischievous smirk on it's face",
+  },
+  {
+    src: sleepy,
+    tags: ["sleepy"],
+    alt: "image of a cat yawning while a human hand scratches it's belly",
+  },
+  {
+    src: surprisedGuilty,
+    tags: ["guilty"],
+    alt: "image of a cat laying in a basket looking half-asleep and surprised to see the camera in a guilty sort of way",
+  },
+  {
+    src: surprised,
+    tags: ["surprised"],
+    alt: "image of a cat with both paws up and looking very surprised at the camera",
+  },
+];
+
+export const allTags = Array.from(
+  new Set<string>(allCatImages.map(({ tags }) => tags).flat())
+);
