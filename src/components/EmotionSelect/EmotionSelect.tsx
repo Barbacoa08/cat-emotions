9859c15ffa1ce6f74dab4638bdfad73a0dd4fda9
Barbacoa08 <sabe12070@gmail.com>
2022-03-26 14:42:19 -0700

    add Emotion Search!  (#20)
    * add Emotion Search!
    
    * update code coverage badges

--- src/components/EmotionSelect/EmotionSelect.tsx
+++ src/components/EmotionSelect/EmotionSelect.tsx
@@ -1,29 +1,82 @@
-import { Box, Grid, GridItem, Heading } from "@chakra-ui/react";
-import { useMemo } from "react";
+import {
+  Box,
+  CloseButton,
+  Container,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+import fuzzysort from "fuzzysort";
+import { useMemo, useState } from "react";
 
 import { CatEmotionImage } from "components/CatEmotionImage";
 import { allCatImages } from "images";
-
 import addYourOwn from "images/add-your-own.png";
 
 export const EmotionSelect = () => {
   // TODO: `useMobileView` (3x3 vs 5x5), but CSS
   // TODO: `typeOfCats: real | anime | pixel | cartoon`
-  // TODO: search by tags
 
-  const items = useMemo(() => {
-    return allCatImages
-      .sort(() => Math.random() - 0.5)
-      .map(({ alt, src, tags }, i) => (
-        <GridItem key={i}>
-          <CatEmotionImage alt={alt} src={src} tags={tags} />
-        </GridItem>
-      ));
-  }, []);
+  const searchableCatImages = useMemo(
+    () =>
+      allCatImages.map((catImage) => ({
+        ...catImage,
+        mergedTags: catImage.tags.join(" "),
+      })),
+    []
+  );
+
+  const [search, setSearch] = useState("");
+  const filteredImages = useMemo(
+    () =>
+      search
+        ? fuzzysort
+            .go(search, searchableCatImages, {
+              key: "mergedTags",
+            })
+            .map((result) => result.obj)
+        : allCatImages,
+    [search, searchableCatImages]
+  );
+
+  const items = useMemo(
+    () =>
+      filteredImages
+        .map(({ alt, src, tags }, i) => (
+          <GridItem key={i}>
+            <CatEmotionImage alt={alt} src={src} tags={tags} />
+          </GridItem>
+        ))
+        .sort(() => Math.random() - 0.5),
+    [filteredImages]
+  );
 
   return (
     <Box role="main">
       <Heading as="h1">Cat/Emotion Selector</Heading>
+
+      <Container>
+        <FormControl>
+          <FormLabel htmlFor="email">Search Emotions</FormLabel>
+          <InputGroup>
+            <Input
+              type="text"
+              placeholder="Type here..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+
+            <InputRightElement>
+              <CloseButton onClick={() => setSearch("")} />
+            </InputRightElement>
+          </InputGroup>
+        </FormControl>
+      </Container>
 
       <Grid
         templateColumns="repeat(3, 1fr)"
@@ -32,6 +85,7 @@ export const EmotionSelect = () => {
         justifyItems="center"
       >
         {items}
+
         <GridItem>
           <CatEmotionImage
             alt="if you can't find a cat that represents your current feels, you can click here to add your own"
