d6472013893f9b8042b3522d84c4b6feb727bf71
barbajoe <sabe12070@gmail.com>
2023-08-19 08:46:28 -0500

    add RUM (#39)

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import * as Cronitor from "@cronitorio/cronitor-rum";
 import netlifyIdentity from "netlify-identity-widget";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -13,6 +14,8 @@ import "./index.css";
 
 (window as any).netlifyIdentity = netlifyIdentity;
 netlifyIdentity.init();
+
+Cronitor.load("f9de2e1cc0b37a02867805b3bbb5d01f");
 
 const globalDefaults: IGlobalState = {
   authenticated: false,
